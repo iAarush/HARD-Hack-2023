8a7199ddad4073a90db11694c57008c9bc295d30
thanhnhanlam <71792534+thanhnhanlam@users.noreply.github.com>
2023-04-15 19:14:37 -0700

    Change value of coefficient 3
    Rename the resistance on board to BOARD_RESISTANCE

--- Analog Temperature Sensor/TempSensor.ino
+++ Analog Temperature Sensor/TempSensor.ino
@@ -1,6 +1,6 @@
 //Analog temperature sensor
 const int thermistorPin = A0;
-const double Rboard = 10000; // Resistor on board
+const double BOARD_RESISTANCE = 10000; // Resistor on board
 const double SHC1 = 0.001129148, SHC2 = 0.000234125, SHC3 = 0.0000000876741;  // Steinhart-Hart coeficients for thermistor
 double resistance, logResistance;
 
@@ -12,6 +12,7 @@ void loop() {
   Serial.print("Temperature: "); 
   Serial.print(getTemperature() - 273.15);
   Serial.println(" C");
+  getTemperature();
   delay(500);
 }
 
@@ -19,7 +20,7 @@ void loop() {
  * Returns the temperature in Kelvin
  */
 double getTemperature() {
-  resistance = Rboard * (1023.0 / (double) analogRead(thermistorPin) - 1.0); // resistance on thermistor
+  resistance = BOARD_RESISTANCE * (1023.0 / (double) analogRead(thermistorPin) - 1.0); // resistance on thermistor
   logResistance = log(resistance);
-  return 1.0 / (SHC1 + SHC2*logResistance + SHC2*pow(logResistance, 3));
+  return 1.0 / (SHC1 + SHC2*logResistance + SHC3*pow(logResistance, 3));
 }
