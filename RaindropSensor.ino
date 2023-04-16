626c0b3a202da0a39a3d938bf2470020e651ce13
thanhnhanlam <71792534+thanhnhanlam@users.noreply.github.com>
2023-04-15 22:27:05 -0700

    Code for the Raindrop Module Sensor
    Code not tested yet, the sensor bag hasn't even been opened.

---
+++ RaindropSensor.ino
@@ -0,0 +1,18 @@
+const int raindropPin = A0; // Uses analog (A0) pin, not digital (D0) pin
+
+void setup() {
+  Serial.begin(9600);  
+}
+void loop() {
+  Serial.println(getRaindropAmount());
+  delay(500);
+}
+
+int getRaindropAmount() {
+  int raindropAmount = analogRead(raindropPin);
+//  if (raindropAmount < 25)
+//    Serial.println("Raining");
+//  else
+//    Serial.println("Not Raining");
+  return raindropAmount;
+}
