3107c1bb2bb2aace2ce264951e7c9362739b0e40
Aarush Mehrotra <43413800+iAarush@users.noreply.github.com>
2023-04-16 04:38:27 -0700

    Update Light sensor readings.ino

--- Light sensor readings/Light sensor readings.ino
+++ Light sensor readings/Light sensor readings.ino
@@ -1,5 +1,13 @@
+int lightMonitor = 750;
+int random_variable;
+int static_variable = 500;
+
 void setup() {
   // put your setup code here, to run once:
+  //pinMode(A0, INPUT);
+  
+  //digitalWrite(IN2_PIN, LOW); 
+
 
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
@@ -9,23 +17,37 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   // reads the input on analog pin A0 (value between 0 and 1023)
-  int analogValue = analogRead(A0);
+  //int lightMonitor = analogRead(A0);
+  lightMonitor = analogRead(A0);
 
-  Serial.print("Analog reading: ");
-  Serial.print(analogValue);   // the raw analog reading
+  //Serial.print("Analog reading: ");
+  Serial.print(lightMonitor);   // the raw analog reading
+  Serial.println(",");
 
   // We'll have a few threshholds, qualitatively determined
-  if (analogValue < 10) {
+  /*
+  if (lightMonitor < 10) {
     Serial.println(" - Dark");
-  } else if (analogValue < 200) {
+  } else if (lightMonitor < 200) {
     Serial.println(" - Dim");
-  } else if (analogValue < 500) {
+  } else if (lightMonitor < 500) {
     Serial.println(" - Light");
-  } else if (analogValue < 800) {
+  } else if (lightMonitor < 800) {
     Serial.println(" - Bright");
   } else {
     Serial.println(" - Very bright");
   }
+*/
 
-  delay(500);
+/*
+  random_variable = random(0, 1000);
+
+  Serial.print("Variable_1:");
+  Serial.print(random_variable);
+  Serial.print(",");
+  Serial.print("Variable_2:");
+  Serial.println(static_variable);
+*/
+
+  delay(1000);
 }
