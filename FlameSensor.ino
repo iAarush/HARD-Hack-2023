f0a23408b8c437503524b0a45a6061dcef75a8ff
thanhnhanlam <71792534+thanhnhanlam@users.noreply.github.com>
2023-04-15 22:08:36 -0700

    Add code for flame sensor
    The sensor was not properly tested for the presence of flame because of the hazard involved.

---
+++ FlameSensor.ino
@@ -0,0 +1,26 @@
+const int flameSensorPin = A0;
+
+void setup()  {
+  Serial.begin(9600);  
+}
+
+void loop() {
+  // read the sensor on analog A0:
+  Serial.println(getFireDistance());
+  delay(500);
+}
+
+/**
+ * Returns the closeness of a fire from 0 (very close) to 1023 (no fire)
+ */
+ int getFireDistance() {
+  int distance = analogRead(flameSensorPin);
+//  if (distance < 41) {
+//    Serial.println("Fire in less than 1.5 ft");
+//  } else if (distance < 82) {
+//    Serial.println("Fire between 1 and 3 ft");
+//  } else {
+//    Serial.println("No fire");
+//  }
+  return distance;
+ }
